95808debe31984eb7f1d3dd4ae76a0150825788a
jiinkyung <jinkyung010310@gmail.com>
2022-05-26 01:24:24 +0900

    django 연동

---
+++ DjangoMobileProject/mobile_project22/mobile_app/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class humanInfo(models.Model) :
+    name = models.CharField(max_length=10)
+    phone_number = models.CharField(max_length=13)
+    address = models.TextField()
+
+    created = models.DateTimeField(auto_now_add=True)
+    class Meta: ordering = ['created']
